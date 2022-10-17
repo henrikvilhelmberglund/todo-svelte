6573e2e200c736b75c19e2a82faf503e1a521c94
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-17 22:37:33 +0200

    add each block binding

--- src/App.svelte
+++ src/App.svelte
@@ -115,6 +115,22 @@
   }
 
   let html = "<p>Write some text!</p>";
+
+  let todos = [
+    { done: false, text: "finish Svelte tutorial" },
+    { done: false, text: "build an app" },
+    { done: false, text: "world domination" },
+  ];
+
+  function add() {
+    todos = todos.concat({ done: false, text: "" });
+  }
+
+  function clear() {
+    todos = todos.filter((t) => !t.done);
+  }
+
+  $: remaining = todos.filter((t) => !t.done).length;
 </script>
 
 <main>
@@ -312,6 +328,22 @@
   <div contenteditable="true" bind:innerHTML={html} />
 
   <pre>{html}</pre>
+
+  <h2>Todos</h2>
+
+  {#each todos as todo}
+    <div class:done={todo.done}>
+      <input type="checkbox" bind:checked={todo.done} />
+
+      <input placeholder="What needs to be done?" bind:value={todo.text} />
+    </div>
+  {/each}
+
+  <p>{remaining} remaining</p>
+
+  <button on:click={add}> Add new </button>
+
+  <button on:click={clear}> Clear completed </button>
 </main>
 
 <style>
@@ -348,5 +380,9 @@
     padding: 0.5em;
     border: 1px solid #eee;
     border-radius: 4px;
+  }
+
+  .done {
+    opacity: 0.4;
   }
 </style>
