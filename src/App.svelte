a0fadc03b2418a70cf5e68d7b48241bc15bfee42
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-17 22:06:38 +0200

    add checkbox and bind

--- src/App.svelte
+++ src/App.svelte
@@ -75,6 +75,11 @@
   }
 
   let nameNew = "world";
+
+  let a = 1;
+  let b = 2;
+
+  let yes = false;
 </script>
 
 <main>
@@ -167,6 +172,33 @@
 
   <input bind:value={nameNew} />
   <h2>Hello {nameNew}!</h2>
+
+  <label>
+    <input type="number" bind:value={a} min="0" max="10" />
+    <input type="range" bind:value={a} min="0" max="10" />
+  </label>
+
+  <label>
+    <input type="number" bind:value={b} min="0" max="10" />
+    <input type="range" bind:value={b} min="0" max="10" />
+  </label>
+
+  <p>{a} + {b} = {a + b}</p>
+
+  <label>
+    <input type="checkbox" bind:checked={yes} />
+    Yes! Send me regular email spam
+  </label>
+
+  {#if yes}
+    <p>Thank you. We will bombard your inbox and sell your personal details.</p>
+  {:else}
+    <p>
+      You must opt in to continue. If you're not paying, you're the product.
+    </p>
+  {/if}
+
+  <button disabled={!yes}> Subscribe </button>
 </main>
 
 <style>
@@ -178,5 +210,13 @@
   div {
     width: 100%;
     height: 100%;
+  }
+
+  label {
+    display: flex;
+  }
+  input,
+  p {
+    margin: 6px;
   }
 </style>
