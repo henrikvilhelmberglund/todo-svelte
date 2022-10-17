7dd2726c3d0d0693d7f1c913f4585e485713f36e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-17 19:30:28 +0200

    add number button

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,24 @@
   let src = "image.gif";
   let string = `this string contains some <strong>HTML!!!</strong>`;
   let count = 0;
-
+  $: doubled = count * 2;
   function incrementCount() {
     count += 1;
   }
+  $: {
+    console.log("the count is " + count);
+    console.log("nice block");
+  }
+  $: if (count >= 10) {
+    console.log("count is dangerously high!");
+    count = 9;
+  }
+  let numbers = [1, 2, 3, 4];
+
+  function addNumber() {
+    numbers = [...numbers, numbers.length + 1];
+  }
+  $: sum = numbers.reduce((t, n) => t + n, 0);
 </script>
 
 <main>
@@ -20,6 +34,10 @@
     Clicked {count}
     {count === 1 ? "time" : "times"}
   </button>
+  <p>{count} doubled is {doubled}</p>
+  <p>{numbers.join(" + ")} = {sum}</p>
+
+  <button on:click={addNumber}> Add a number </button>
 </main>
 
 <style>
